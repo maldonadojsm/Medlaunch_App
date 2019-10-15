3608eadc86d3432ea332545aeb6324a83be0926f
Juan Maldonado <zahza@Juans-MacBook-Pro.local>
2019-10-15 01:08:32 -0500

    Medlaunch Stable Version

--- front_end/src/components/leads/Form.js
+++ front_end/src/components/leads/Form.js
@@ -5,11 +5,6 @@ export class Form extends Component {
     return (
       <div>
         <h1>Add Lead</h1>
-        <form class="form-inline my-2 my-lg-0">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-            Search
-          </button>
-        </form>
       </div>
     );
   }
